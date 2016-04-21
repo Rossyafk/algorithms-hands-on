aa87c4d468b7f29a31f7dc6e904262feaee21a38
Oscar Ros <oscar.ros@scytl.com>
2016-04-21 16:07:42 +0200

    Added BynaryGap algorithm.

--- src/test/java/com/rizandoelrizo/algorithms/codility/lesson1/BinaryGapShould.java
+++ src/test/java/com/rizandoelrizo/algorithms/codility/lesson1/BinaryGapShould.java
@@ -10,8 +10,38 @@ public class BinaryGapShould {
     private final BinaryGap sut = new BinaryGap();
 
     @Test
-    public void return_zero() {
-        assertTrue(sut.calculateBinaryGapFor(0) == 0);
+    public void pass_with_the_min_value() {
+        assertTrue(sut.calculateBiggestBinaryGapFor(1) == 0);
+    }
+
+    @Test
+    public void pass_with_the_max_value() {
+        assertTrue(sut.calculateBiggestBinaryGapFor(2_147_483_647) == 0);
+    }
+
+    @Test
+    public void pass_with_the_given_example_v1() {
+        assertTrue(sut.calculateBiggestBinaryGapFor(9) == 2);
+    }
+
+    @Test
+    public void pass_with_the_given_example_v2() {
+        assertTrue(sut.calculateBiggestBinaryGapFor(529) == 4);
+    }
+
+    @Test
+    public void pass_with_the_given_example_v3() {
+        assertTrue(sut.calculateBiggestBinaryGapFor(529) == 4);
+    }
+
+    @Test
+    public void pass_with_the_given_example_v4() {
+        assertTrue(sut.calculateBiggestBinaryGapFor(20) == 1);
+    }
+
+    @Test
+    public void pass_with_the_given_example_v5() {
+        assertTrue(sut.calculateBiggestBinaryGapFor(15) == 0);
     }
 
 }
