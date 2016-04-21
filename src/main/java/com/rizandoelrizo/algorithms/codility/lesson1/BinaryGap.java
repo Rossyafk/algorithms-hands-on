740fbd03cbe2a1c2b6c6370e349171e0a784bc67
Oscar Ros <oscar.ros@scytl.com>
2016-04-21 15:29:47 +0200

    Adding logging capabilities.

--- src/main/java/com/rizandoelrizo/algorithms/codility/lesson1/BinaryGap.java
+++ src/main/java/com/rizandoelrizo/algorithms/codility/lesson1/BinaryGap.java
@@ -1,9 +1,14 @@
 package com.rizandoelrizo.algorithms.codility.lesson1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Find longest sequence of zeros in binary representation of an integer.
  */
 public class BinaryGap {
+
+    private static final Logger logger = LoggerFactory.getLogger(BinaryGap.class);
 
     public int calculateBinaryGapFor(int integer) {
         return 0;
